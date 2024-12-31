a542bf39ffa1e71734a16d506f80ff1d59ff1f98
Carlo Maximo <cmaximo15@gmail.com>
2024-12-30 21:00:07 -0500

    WIP: fixed more issues with the sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "vollkorn": ['Vollkorn', 'serif']
+        "oswald": ['Oswald', 'sans-serif'],
+        "inter": ['Inter', 'sans-serif']
       }
     },
   },
